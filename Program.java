6f87c4c2ea30049bcfaa226e2c7762cbbafec8b2
DevGuilhermeTempesta <guilhermeftempesta@gmail.com>
2025-04-26 18:06:24 -0300

    atualizaçao na classe curriculo e teste realizado

--- Program.java
+++ Program.java
@@ -5,11 +5,20 @@ import projetos.OnlineCV.model.Curriculo;
 public class Program {
     public static void main(String[] args) {
         Curriculo c = new Curriculo();
-        c.adcionarExperiencia("desenvolvimento de programas");
-        c.adcionarFormacao("ciencias da computaçao pelo centro universitario de brasilia");
-        c.adcionarHabilidades("java");
-        c.adcionarFormacao("medicina pela universidade nacional de brasilia");
-
+        c.adicionarExperiencia("desenvolvimento de programas");
+        c.adicionarFormacao("ciencias da computaçao pelo centro universitario de brasilia");
+        c.adicionarHabilidades("java");
         c.exibir();
+
+        String serializado = c.serializar();
+        System.out.println("serializando o curriculo:");
+        System.out.println(serializado);
+
+        System.out.println();
+        System.out.println("desserializando o curriculo:");
+
+        Curriculo novoCv = Curriculo.desserializar(serializado);
+        novoCv.exibir();
+
     }
 }
--- model/Curriculo.java
+++ model/Curriculo.java
@@ -9,13 +9,13 @@ public class Curriculo {
     private List<String> experiencias = new ArrayList<>();
     private List<String> habilidades = new ArrayList<>();
 
-    public void adcionarFormacao(String f){
+    public void adicionarFormacao(String f){
         formacoes.add(f);
     }
-    public void adcionarExperiencia(String e){
+    public void adicionarExperiencia(String e){
         experiencias.add(e);
     }
-    public void adcionarHabilidades(String h){
+    public void adicionarHabilidades(String h){
         habilidades.add(h);
     }
 
@@ -27,36 +27,39 @@ public class Curriculo {
         experiencias.forEach(e -> System.out.println("- "+e));
         System.out.println("habilidades: ");
         habilidades.forEach(h -> System.out.println("- "+h));
+        System.out.println("=================");
     }
 //transformar o objeto curriculo em uma string formatada para armazenamento em arquivo ou banco de dados
 
     public String serializar(){
-        return  String.join(","+formacoes) + "|" + String.join(","+experiencias) + "|" + String.join(","+habilidades);
+        return  String.join(",", formacoes) + "|" +
+                String.join(",", experiencias) + "|" +
+                String.join(",", habilidades);
     }
 //converte a string serializada de volta em um objeto curriculo
 
     public static Curriculo desserializar(String data){
-        String fields[] = data.split("\\|");
+        String[] fields = data.split("\\|");
         Curriculo c = new Curriculo();
 
-        if(fields.length>0){
+        if(fields.length>0 && !fields[0].isEmpty()){
             for(String f: fields[0].split(",")){
                 if (!f.isEmpty()){
-                    c.adcionarFormacao(f);
+                    c.adicionarFormacao(f.trim());
                 }
             }
         }
-        if (fields.length> 1){
+        if (fields.length> 1 && !fields[1].isEmpty()){
             for (String e: fields[1].split(",")){
                 if (!e.isEmpty()){
-                    c.adcionarExperiencia(e);
+                    c.adicionarExperiencia(e.trim());
                 }
             }
         }
-        if(fields.length>2){
+        if(fields.length>2 && !fields[2].isEmpty()){
             for (String h: fields[2].split(",")){
                 if(!h.isEmpty()){
-                    c.adcionarHabilidades(h);
+                    c.adicionarHabilidades(h.trim());
                 }
             }
         }
